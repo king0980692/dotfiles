81d7133ed487b0bfe499c30ebe3c522dd4919b3e
Leon Chang <king0980692@gmail.com>
2021-04-08 00:41:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
+echo -ne '\n' | sudo add-apt-repository ppa:neovim-ppa/stable
+
 sudo apt-get update
 
 sudo apt-get -y install neovim
 
 sudo apt-get -y install zsh
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+yes | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-git clone --bare https://github.com/king0980692/dotfiles.git $HOME/.dotfiles
+sudo apt install -y python4-pip
+
+git clone --bare https://github.com/king0980693/dotfiles.git $HOME/.dotfiles
+
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+rm ~/.zshrc
+
+dotfiles checkout
+
+
+                                                                                                                1,1           All
