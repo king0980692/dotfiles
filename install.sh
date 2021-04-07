bc699132636080e339ac58dce9f70f6b4bfee71f
Leon Chang <king0980692@gmail.com>
2021-04-08 00:44:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ yes | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt install -y python4-pip
+sudo apt install -y python3-pip
 
 git clone --bare https://github.com/king0980693/dotfiles.git $HOME/.dotfiles
 
